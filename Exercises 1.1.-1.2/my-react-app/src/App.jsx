37ae9dc52416bc6f3be2cc884aafe7efdd83313b
Sxnny-s <ethsok@protonmail.com>
2025-02-09 19:07:36 -0500

    react project

--- Exercises 1.1.-1.2/my-react-app/src/App.jsx
+++ Exercises 1.1.-1.2/my-react-app/src/App.jsx
@@ -1,63 +1,13 @@
 import { use, useState } from 'react'
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
+const Title = () => {
+ <h1>Give Feedback</h1>
 }
 
-const Button = (props) => { 
 
-  console.log('props value is', props)
-  const { onClick, text } = props
-  return (
-    <button onClick={onClick}>
-      {text}
-    </button>
-  )
-}
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-
-  const [allClicks, setAll] = useState([])
-  const [total, increment] = useState(0)
-  
-
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    increment(total + 1)
-
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    increment(total + 1)
-    setRight(right + 1)
-  }
-
-  return (
-    <div>
-      {left}
-      <Button onClick={handleLeftClick} text='left' />
-      <Button onClick={handleRightClick} text='right' />
-      {right}
-
-      <History allClicks={allClicks} />
-    </div>
-  )
+ <Title />
 }
 
 export default App
