cd4a3cb1ab36e7b14ac075bf4e96d184991708db
sxnny-s <115429120+Sxnny-s@users.noreply.github.com>
2025-03-29 18:57:01 -0400

    new

--- FSO PT 6/unicafe-redux/src/main.jsx
+++ FSO PT 6/unicafe-redux/src/main.jsx
@@ -6,22 +6,22 @@ import reducer from './reducer'
 
 const store = createStore(reducer)
 
-const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
+store.subscribe(() => {
+  const storeNow = store.getState()
+  console.log(storeNow)
+})
 
+const App = () => {
+  
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={e => store.dispatch({ type: 'GOOD'})}>good</button> 
+      <button onClick={e => store.dispatch({ type: 'OK'})}>ok</button> 
+      <button onClick={e => store.dispatch({ type: 'BAD'})}>bad</button>
+      <button onClick={e => store.dispatch({ type: 'ZERO'})}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
